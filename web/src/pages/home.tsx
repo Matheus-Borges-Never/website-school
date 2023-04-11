1909298f4ba4174163b5a7b339d4fc2b155ef640
Matheus Borges <matheusborgespaulino@gmail.com>
2023-04-11 14:06:50 -0300

    valores feito

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Slide from '../components/slide';
+import Values from '../components/values';
 
 function Home() {
   return (
-    <Slide />
+    <React.StrictMode>
+      <Slide />
+      <Values />
+    </React.StrictMode>
   );
 }
 
