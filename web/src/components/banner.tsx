d0d48d9101ee91bf91d4b9edbac0bb86c796600e
Matheus Borges <matheusborgespaulino@gmail.com>
2023-04-12 12:27:21 -0300

    imagens pilares editadas

--- web/src/components/banner.tsx
+++ web/src/components/banner.tsx
@@ -15,8 +15,8 @@ function Banner() {
         <div className="container">
           <div className="row row-cols-1 row-cols-md-3 g-4">
             <div className="col">
+              <div className="imgMission"></div>
               <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">Card title</h5>
                   <p className="card-text">
