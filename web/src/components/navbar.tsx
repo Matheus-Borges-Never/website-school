84856d483c006e1adba6b6766eee7d037f4e24ac
Matheus Borges <matheusborgespaulino@gmail.com>
2023-04-13 23:53:17 -0300

    navbar responsivo

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -5,18 +5,17 @@ function Navbar() {
   return (
     <div>
         <nav className="navbar navbar-expand-lg navbarContent justify-content-center">
-            <div className="container-fluid">                    
-                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <div className="container-fluid">              
+                <div className="logo">
+                    <img src="https://monteirolobatofranca.com.br/assets/img/MONTEIRO_LOBATO_2021_350X200_SITE%20logo.png" width="208" alt="Monteiro Lobato logo" />
+                </div>   
+                <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav navAlign align-middle align-items-center me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <div className="logo">
-                                <img src="https://monteirolobatofranca.com.br/assets/img/MONTEIRO_LOBATO_2021_350X200_SITE%20logo.png" width="208" alt="Monteiro Lobato logo" />
-                            </div>
-                        </li>
-                        <li className="nav-item navText">
+                        <br />
+                        <li className="nav-item text-center">
                             <h1 className="nav-link active text-light">Home</h1>
                         </li>
                         <li className="nav-item">
